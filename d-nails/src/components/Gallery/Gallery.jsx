7a7bd6fc29e072bd8ef62d11340c362c0eb81e4e
Michal Piechowiak <nilfenheim@gmail.com>
2022-04-11 19:37:45 +0200

    add: image enlarge in gallery

--- d-nails/src/components/Gallery/Gallery.jsx
+++ d-nails/src/components/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import bemCssModules from 'bem-css-modules';
 
 import { default as GalleryStyles } from './Gallery.module.sass';
@@ -9,11 +9,17 @@ const style = bemCssModules(GalleryStyles);
 
 const Gallery = (props) => {
 
+    // const [galleryMode, setGalleryMode] = useState('masonry');
+    const [currentlyEnlarged, setCurrentlyEnlarged] = useState(null);
+
     const imageNames = new Array(14).fill(placeholder)
-    const images = imageNames.map((el, idx) => <GalleryImage key={el + idx} image={el} />)
+    const images = imageNames.map((el, idx) => <GalleryImage key={el + idx} image={el} index={idx} enlarge={false} active={currentlyEnlarged} changeActive={setCurrentlyEnlarged} />)
+
+    const mode = currentlyEnlarged === null ? style() : style({ enlarged: true });
 
     return (
-        <div className={style()}>
+        <div className={mode}>
+            {currentlyEnlarged !== null ? <GalleryImage image={imageNames[currentlyEnlarged]} index={currentlyEnlarged} enlarge={true} active={currentlyEnlarged} changeActive={setCurrentlyEnlarged} /> : null}
             {images}
         </div>
     );
--- d-nails/src/components/GalleryImage/GalleryImage.jsx
+++ d-nails/src/components/GalleryImage/GalleryImage.jsx
@@ -7,9 +7,20 @@ const style = bemCssModules(GalleryImageStyles);
 
 const GalleryImage = (props) => {
 
+    const imageGridStyle = props.active !== null ? style(props.enlarge ? { enlarged: true } : { miniature: true }) : style({ normal: true });
+
+    const onClickHandler = () => {
+        if (props.enlarge) {
+            props.changeActive(null)
+        } else {
+            props.changeActive(props.index)
+        }
+
+    }
+
     return (
-        <div className={style()}>
-            <img src={props.image} className={style('imag')} alt="" />
+        <div className={imageGridStyle} onClick={onClickHandler}>
+            <img src={props.image} className={style('imag', { active: props.active === props.index })} alt="" />
         </div>);
 }
 

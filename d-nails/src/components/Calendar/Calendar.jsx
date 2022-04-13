071764401894ce8c2775694c6f3ac6862d4d410f
Michal Piechowiak <nilfenheim@gmail.com>
2022-04-13 17:28:21 +0200

    edit: calendar functionality

--- d-nails/src/components/Calendar/Calendar.jsx
+++ d-nails/src/components/Calendar/Calendar.jsx
@@ -6,19 +6,27 @@ import { default as CalendarStyles } from './Calendar.module.sass';
 const style = bemCssModules(CalendarStyles);
 
 const Calendar = () => {
+
+    const days = Array.from(Array(31), (_, i) => <div className={style('day')}>{i + 1}</div>)
+
     return (
         <div className={style()}>
-            <div className={style('month')}></div>
-            <div className={style('week')}>
-                <div className={style('weekday')}>PN</div>
-                <div className={style('weekday')}>WT</div>
-                <div className={style('weekday')}>ŚR</div>
-                <div className={style('weekday')}>CZ</div>
-                <div className={style('weekday')}>PT</div>
-                <div className={style('weekday')}>SO</div>
-                <div className={style('weekday')}>N</div>
+            <div className={style('month')}>
+                <div className={style('chosen_month', { inactive_month: false })}>OBECNY</div>
+                <div className={style('chosen_month', { inactive_month: true })}>NASTEPNY</div>
             </div>
-            <div className={style('days')}></div>
+            <div className={style('week')}>
+                <div className={style('weekday')}>PON</div>
+                <div className={style('weekday')}>WTO</div>
+                <div className={style('weekday')}>ŚRO</div>
+                <div className={style('weekday')}>CZW</div>
+                <div className={style('weekday')}>PIĄ</div>
+                <div className={style('weekday')}>SOB</div>
+                <div className={style('weekday')}>NIE</div>
+            </div>
+            <div className={style('days')}>
+                {days}
+            </div>
         </div >
     );
 }

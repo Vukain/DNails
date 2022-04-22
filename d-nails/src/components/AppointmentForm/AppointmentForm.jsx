46cadf60609d162c9b071cb41c9063889d5d1a44
Michal Piechowiak <nilfenheim@gmail.com>
2022-04-22 16:39:56 +0200

    edit: form styling

--- d-nails/src/components/AppointmentForm/AppointmentForm.jsx
+++ d-nails/src/components/AppointmentForm/AppointmentForm.jsx
@@ -10,10 +10,10 @@ const AppointmentForm = () => {
         <div className={style()}>
             <form className={style('form')} action="submit">
 
-                <input className={style('input')} type="text" placeholder='IMIĘ' />
-                <input className={style('input')} type="text" placeholder='NAZWISKO' />
-                <input className={style('input')} type="email" placeholder='EMAIL' />
-                <input className={style('input')} type="datetime-local" placeholder='DATA' />
+                <input className={style('input', { upper_left: true })} type="text" placeholder='IMIĘ' />
+                <input className={style('input', { upper_right: true })} type="text" placeholder='NAZWISKO' />
+                <input className={style('input', { inner: true })} type="email" placeholder='EMAIL' />
+                <input className={style('input', { inner: true })} type="datetime-local" placeholder='DATA' />
                 <textarea className={style('message')} name="" id="" cols="30" rows="10"></textarea>
                 <button className={style('button')}>wyślij</button>
             </form>

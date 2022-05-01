4e96fcd62c9cec84330d3f83827eea303b24f748
Michal Piechowiak <nilfenheim@gmail.com>
2022-05-01 11:41:28 +0200

    fix: empty day calculation

--- d-nails/src/components/Calendar/Calendar.jsx
+++ d-nails/src/components/Calendar/Calendar.jsx
@@ -22,7 +22,7 @@ const Calendar = () => {
     const emptyDaysCurrent = Array.from(Array(startDayCurrent > 1 ? startDayCurrent - 1 : 6), (_, i) => <div key={"empty" + i} className={style('day', { not_empty: false })}></div>);
 
     const daysNext = Array.from(Array(nextMonth.daysInMonth()), (_, i) => <div key={i} onClick={() => selectDayHandler(i)} className={style('day', { not_empty: true, selected: i + 1 === selectedDay, busy: i % 3 === 0, unavailable: i % 5 === 0, })}>{i + 1}</div>);
-    const emptyDaysNext = Array.from(Array(startDayNext > 1 ? startDayCurrent - 1 : 6), (_, i) => <div key={"empty" + i} className={style('day', { not_empty: false })}></div>);
+    const emptyDaysNext = Array.from(Array(startDayNext > 1 ? startDayNext - 1 : 6), (_, i) => <div key={"empty" + i} className={style('day', { not_empty: false })}></div>);
 
     const displayedDays = activeMonth === 0 ? [...emptyDaysCurrent, ...daysCurrent] : [...emptyDaysNext, ...daysNext];
 
@@ -43,10 +43,12 @@ const Calendar = () => {
 
     return (
         <div className={style()}>
+
             <div className={style('month')}>
                 <div onClick={() => { monthHandler(0) }} className={style('chosen_month', { inactive_month: activeMonth !== 0 })}>{months[currentMonth.month()]}</div>
                 <div onClick={() => { monthHandler(1) }} className={style('chosen_month', { inactive_month: activeMonth !== 1 })}>{months[nextMonth.month()]}</div>
             </div>
+
             <div className={style('week')}>
                 <div className={style('weekday')}>PON</div>
                 <div className={style('weekday')}>WTO</div>
@@ -55,22 +57,20 @@ const Calendar = () => {
                 <div className={style('weekday')}>PIĄ</div>
                 <div className={style('weekday')}>SOB</div>
                 <div className={style('weekday')}>NIE</div>
-                <div className={style('weekday')}>GODZ</div>
             </div>
-            <div className={style('days_hours')}>
 
-                <div className={style('days')}>
-                    {displayedDays}
-                </div>
+            <div className={style('days')}>
+                {displayedDays}
+            </div>
 
-                <div className={style('hours')}>
-                    <div className={style('hour')}>8</div>
-                    <div className={style('hour')}>10</div>
-                    <div className={style('hour')}>12</div>
-                    <div className={style('hour')}>14</div>
-                    <div className={style('hour')}>16</div>
-                    <div className={style('hour')}>18</div>
-                </div>
+            <div className={style('hours')}>
+                <div className={style('hour_label')}>GODZ</div>
+                <div className={style('hour')}>8</div>
+                <div className={style('hour')}>10</div>
+                <div className={style('hour')}>12</div>
+                <div className={style('hour')}>14</div>
+                <div className={style('hour')}>16</div>
+                <div className={style('hour')}>18</div>
             </div>
         </div >
     );

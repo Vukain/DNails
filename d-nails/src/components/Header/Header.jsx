aa4e8f656c9e4317c6ad0e1c089a41af853cf184
Michal Piechowiak <nilfenheim@gmail.com>
2022-05-21 16:21:17 +0200

    add: wip mobile image

--- d-nails/src/components/Header/Header.jsx
+++ d-nails/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import bemCssModules from 'bem-css-modules';
 import { default as HeaderStyles } from './Header.module.sass';
 
 import { ReactComponent as HeaderImage } from '../../media/header_nails.svg';
+import { ReactComponent as HeaderImagePort } from '../../media/header_nails_port.svg';
 
 const style = bemCssModules(HeaderStyles);
 
@@ -11,6 +12,7 @@ const Header = forwardRef((props, ref) => {
     return (
         <header className={style()} ref={ref}>
             <HeaderImage className={style('header_image')} preserveAspectRatio='none' />
+            <HeaderImagePort className={style('header_image', { 'port': true })} preserveAspectRatio='none' />
         </header>
     );
 })

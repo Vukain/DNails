40bbf0ae65a4736c5c7f3fcfc691132d2e2c3ea0
Michal Piechowiak <nilfenheim@gmail.com>
2023-01-27 16:46:28 +0100

    fix: scroll bugs and delays

--- d-nails/src/App.jsx
+++ d-nails/src/App.jsx
@@ -15,7 +15,7 @@ import Modal from './layout/Modal/Modal';
 
 function App() {
 
-  const { showModal, currentSectionRef, setCurrentSection, setSectionRefs, setCurrentLevel  } = useContext(AppContext);
+  const { showModal, currentSectionRef, setCurrentSection, setSectionRefs, setCurrentLevel } = useContext(AppContext);
 
   const isThrottled = useRef(false);
 
@@ -25,8 +25,10 @@ function App() {
   const thirdSectionRef = useRef(null);
   const fourthSectionRef = useRef(null);
 
-  const [touchStart, setTouchStart] = useState(0);
-  const [touchEnd, setTouchEnd] = useState(0);
+  const [touchStartY, setTouchStartY] = useState(0);
+  const [touchEndY, setTouchEndY] = useState(0);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
 
 
   const sectionChanger = useCallback((direction) => {
@@ -34,67 +36,86 @@ function App() {
 
     if (direction === 'up' && currentSectionRef.current > 0) {
       currentSectionRef.current -= 1;
+      isThrottled.current = true;
       setCurrentSection(currentSectionRef.current);
       if (currentSectionRef.current === 0) {
         setCurrentLevel(1);
       };
     } else if (direction === 'down' && currentSectionRef.current < sections.length - 1) {
       currentSectionRef.current += 1;
+      isThrottled.current = true;
       setCurrentSection(currentSectionRef.current);
       setCurrentLevel(2);
     };
     sections[currentSectionRef.current].scrollIntoView({ behavior: 'smooth', inline: 'center', block: 'start' });
+
   }, [currentSectionRef, setCurrentSection, setCurrentLevel]);
 
   const scrollHandler = (e) => {
-    console.log(isThrottled.current);
     if (!isThrottled.current) {
-      console.log('Scrolling, setting scroll throttle')
-      isThrottled.current = true;
       if (e.deltaY > 0) {
-        // sectionChanger('down');
-        setTimeout(() => {
-          sectionChanger('down');
-        }, 700);
+        sectionChanger('down');
       } else {
-        // sectionChanger('up');
-        setTimeout(() => {
-          sectionChanger('up');
-        }, 700);
+        sectionChanger('up');
       };
 
       setTimeout(() => {
         isThrottled.current = false;
-        console.log('Lifting scroll throttle')
-      }, 1000);
+      }, 800);
     };
   };
 
   const handleTouchStart = (e) => {
-    setTouchStart(e.targetTouches[0].clientY);
-    setTouchEnd(e.targetTouches[0].clientY);
+    setTouchStartY(e.targetTouches[0].clientY);
+    setTouchEndY(e.targetTouches[0].clientY);
+    setTouchStartX(e.targetTouches[0].clientX);
+    setTouchEndX(e.targetTouches[0].clientX);
   };
 
   const handleTouchMove = (e) => {
-      setTouchEnd(e.targetTouches[0].clientY);
+    setTouchEndY(e.targetTouches[0].clientY);
+    setTouchEndX(e.targetTouches[0].clientX);
   };
 
   const handleTouchEnd = () => {
-      if (touchStart - touchEnd > 160) {
-        setTimeout(() => {
-          sectionChanger('down');
-        }, 100);
-      };
 
-      if (touchStart - touchEnd < -160) {
-        setTimeout(() => {
-          sectionChanger('up');
-        }, 100);
+    const yDistance = Math.abs(touchStartY - touchEndY);
+    const xDistance = Math.abs(touchStartX - touchEndX);
+
+    // Set minimum screen distance percentage to 10%
+    const distanceTresholdY = window.innerHeight / 10;
+    const distanceTresholdX = window.innerWidth / 10;
+
+    // Set touch direction according to screen orientation and prevent accidental scrolls
+    if (window.matchMedia("(orientation: portrait)").matches) {
+      if ((yDistance > 3 * xDistance) && (yDistance > distanceTresholdY)) {
+        if (touchStartY > touchEndY) {
+          setTimeout(() => {
+            sectionChanger('down');
+          }, 100);
+        } else {
+          setTimeout(() => {
+            sectionChanger('up');
+          }, 100);
+        };
       };
+    } else {
+      if ((xDistance > 3 * yDistance) && (xDistance > distanceTresholdX)) {
+        if (touchStartX > touchEndX) {
+          setTimeout(() => {
+            sectionChanger('down');
+          }, 100);
+        } else {
+          setTimeout(() => {
+            sectionChanger('up');
+          }, 100);
+        };
+      };
+    };
   };
 
   const keyDownHandler = useCallback((e) => {
-    if (e.key === 'ArrowDown') { 
+    if (e.key === 'ArrowDown') {
       sectionChanger('down');
     } else if (e.key === 'ArrowUp') {
       sectionChanger('up');
@@ -107,12 +128,16 @@ function App() {
     document.addEventListener('keydown', keyDownHandler);
   }, [setSectionRefs, keyDownHandler]);
 
+  useEffect(() => {
+    window.addEventListener('wheel', (e) => { e.preventDefault() }, { passive: false });
+  }, [])
+
   return (
     <div className="App" onWheel={scrollHandler} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
 
       <Header ref={headerRef} />
 
-      <Navigation/>
+      <Navigation />
 
       <Section ref={firstSectionRef} position='first'>
         <Prices />
@@ -130,7 +155,7 @@ function App() {
         <Gallery />
       </Section>
 
-      {showModal ? <Modal/> : null}
+      {showModal ? <Modal /> : null}
     </div>
   );
 };

13e373b82e1ecbdd3f890ce896d317bb62c29a81
Michal Piechowiak <nilfenheim@gmail.com>
2023-01-28 14:40:01 +0100

    optimize: optimize images

--- d-nails/src/components/Gallery/GalleryImage/GalleryImage.jsx
+++ d-nails/src/components/Gallery/GalleryImage/GalleryImage.jsx
@@ -14,7 +14,6 @@ const GalleryImage = ({ enlarge, active, image, index, changeActive, top, bottom
             {!enlarge ? <img src={image.mini_bw} className={style('imag', { active: active === index, enlarge: enlarge, top: top, bottom: bottom })} loading='lazy' alt={`zdjęcie pomalowanych paznokci ${index + 1}`} /> : null}
             {!enlarge ?
                 <img src={image.mini} className={style('imag', { color: true, active: active === index, enlarge: enlarge, top: top, bottom: bottom })} loading="lazy" alt={`zdjęcie pomalowanych paznokci ${index + 1}`} /> :
-                // <img src={image.full} className={style('imag', { color: true, active: active === index, enlarge: enlarge, top: top, bottom: bottom })} loading="lazy" alt={`zdjęcie pomalowanych paznokci ${index + 1}`} />
                 <img className={style('imag', { color: true, active: active === index, enlarge: enlarge, top: top, bottom: bottom })} loading="lazy"
                     srcSet={`${image.mini} 600w, ${image.small} 750w, ${image.medium} 900w, ${image.big} 1200w, ${image.full} 1500w`}
                     sizes={'(orientation: portrait) 100vw, 40vw'}

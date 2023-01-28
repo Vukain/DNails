b0bbf73b712addf7a507b3cbb7fc05ffe017b247
Michal Piechowiak <nilfenheim@gmail.com>
2023-01-28 14:37:50 +0100

    optimization: optimize images

--- d-nails/src/components/Gallery/Gallery.jsx
+++ d-nails/src/components/Gallery/Gallery.jsx
@@ -6,12 +6,22 @@ import GalleryStyles from './Gallery.module.sass';
 import GalleryImage from './GalleryImage/GalleryImage';
 
 import {
-    nail_image_01, nail_image_01_mini, nail_image_01_bw_mini, nail_image_02, nail_image_02_mini, nail_image_02_bw_mini, nail_image_03, nail_image_03_mini, nail_image_03_bw_mini,
-    nail_image_04, nail_image_04_mini, nail_image_04_bw_mini, nail_image_05, nail_image_05_mini, nail_image_05_bw_mini, nail_image_06, nail_image_06_mini, nail_image_06_bw_mini,
-    nail_image_07, nail_image_07_mini, nail_image_07_bw_mini, nail_image_08, nail_image_08_mini, nail_image_08_bw_mini, nail_image_09, nail_image_09_mini, nail_image_09_bw_mini,
-    nail_image_10, nail_image_10_mini, nail_image_10_bw_mini, nail_image_11, nail_image_11_mini, nail_image_11_bw_mini, nail_image_12, nail_image_12_mini, nail_image_12_bw_mini,
-    nail_image_13, nail_image_13_mini, nail_image_13_bw_mini, nail_image_14, nail_image_14_mini, nail_image_14_bw_mini, nail_image_15, nail_image_15_mini, nail_image_15_bw_mini
-} from '../../media';
+    nail_image_01, nail_image_01_big, nail_image_01_medium, nail_image_01_small, nail_image_01_mini, nail_image_01_mini_bw,
+    nail_image_02, nail_image_02_big, nail_image_02_medium, nail_image_02_small, nail_image_02_mini, nail_image_02_mini_bw,
+    nail_image_03, nail_image_03_big, nail_image_03_medium, nail_image_03_small, nail_image_03_mini, nail_image_03_mini_bw,
+    nail_image_04, nail_image_04_big, nail_image_04_medium, nail_image_04_small, nail_image_04_mini, nail_image_04_mini_bw,
+    nail_image_05, nail_image_05_big, nail_image_05_medium, nail_image_05_small, nail_image_05_mini, nail_image_05_mini_bw,
+    nail_image_06, nail_image_06_big, nail_image_06_medium, nail_image_06_small, nail_image_06_mini, nail_image_06_mini_bw,
+    nail_image_07, nail_image_07_big, nail_image_07_medium, nail_image_07_small, nail_image_07_mini, nail_image_07_mini_bw,
+    nail_image_08, nail_image_08_big, nail_image_08_medium, nail_image_08_small, nail_image_08_mini, nail_image_08_mini_bw,
+    nail_image_09, nail_image_09_big, nail_image_09_medium, nail_image_09_small, nail_image_09_mini, nail_image_09_mini_bw,
+    nail_image_10, nail_image_10_big, nail_image_10_medium, nail_image_10_small, nail_image_10_mini, nail_image_10_mini_bw,
+    nail_image_11, nail_image_11_big, nail_image_11_medium, nail_image_11_small, nail_image_11_mini, nail_image_11_mini_bw,
+    nail_image_12, nail_image_12_big, nail_image_12_medium, nail_image_12_small, nail_image_12_mini, nail_image_12_mini_bw,
+    nail_image_13, nail_image_13_big, nail_image_13_medium, nail_image_13_small, nail_image_13_mini, nail_image_13_mini_bw,
+    nail_image_14, nail_image_14_big, nail_image_14_medium, nail_image_14_small, nail_image_14_mini, nail_image_14_mini_bw,
+    nail_image_15, nail_image_15_big, nail_image_15_medium, nail_image_15_small, nail_image_15_mini, nail_image_15_mini_bw,
+} from '../../media/nail_photos';
 
 const style = bemCssModules(GalleryStyles);
 
@@ -41,12 +51,23 @@ const Gallery = () => {
     };
 
     // const imageNames = new Array(14).fill([nail_image_01_bw, nail_image_01])
-    const imageNames = [[nail_image_01_mini, nail_image_01_bw_mini, nail_image_01], [nail_image_02_mini, nail_image_02_bw_mini, nail_image_02], [nail_image_03_mini, nail_image_03_bw_mini, nail_image_03],
-    [nail_image_04_mini, nail_image_04_bw_mini, nail_image_04], [nail_image_05_mini, nail_image_05_bw_mini, nail_image_05], [nail_image_06_mini, nail_image_06_bw_mini, nail_image_06],
-    [nail_image_07_mini, nail_image_07_bw_mini, nail_image_07], [nail_image_08_mini, nail_image_08_bw_mini, nail_image_08], [nail_image_09_mini, nail_image_09_bw_mini, nail_image_09],
-    [nail_image_10_mini, nail_image_10_bw_mini, nail_image_10], [nail_image_11_mini, nail_image_11_bw_mini, nail_image_11], [nail_image_12_mini, nail_image_12_bw_mini, nail_image_12],
-    [nail_image_13_mini, nail_image_13_bw_mini, nail_image_13], [nail_image_14_mini, nail_image_14_bw_mini, nail_image_14], [nail_image_15_mini, nail_image_15_bw_mini, nail_image_15]
+    const imageNames = [{ full: nail_image_01, big: nail_image_01_big, medium: nail_image_01_medium, small: nail_image_01_small, mini: nail_image_01_mini, mini_bw: nail_image_01_mini_bw },
+    { full: nail_image_02, big: nail_image_02_big, medium: nail_image_02_medium, small: nail_image_02_small, mini: nail_image_02_mini, mini_bw: nail_image_02_mini_bw },
+    { full: nail_image_03, big: nail_image_03_big, medium: nail_image_03_medium, small: nail_image_03_small, mini: nail_image_03_mini, mini_bw: nail_image_03_mini_bw },
+    { full: nail_image_04, big: nail_image_04_big, medium: nail_image_04_medium, small: nail_image_04_small, mini: nail_image_04_mini, mini_bw: nail_image_04_mini_bw },
+    { full: nail_image_05, big: nail_image_05_big, medium: nail_image_05_medium, small: nail_image_05_small, mini: nail_image_05_mini, mini_bw: nail_image_05_mini_bw },
+    { full: nail_image_06, big: nail_image_06_big, medium: nail_image_06_medium, small: nail_image_06_small, mini: nail_image_06_mini, mini_bw: nail_image_06_mini_bw },
+    { full: nail_image_07, big: nail_image_07_big, medium: nail_image_07_medium, small: nail_image_07_small, mini: nail_image_07_mini, mini_bw: nail_image_07_mini_bw },
+    { full: nail_image_08, big: nail_image_08_big, medium: nail_image_08_medium, small: nail_image_08_small, mini: nail_image_08_mini, mini_bw: nail_image_08_mini_bw },
+    { full: nail_image_09, big: nail_image_09_big, medium: nail_image_09_medium, small: nail_image_09_small, mini: nail_image_09_mini, mini_bw: nail_image_09_mini_bw },
+    { full: nail_image_10, big: nail_image_10_big, medium: nail_image_10_medium, small: nail_image_10_small, mini: nail_image_10_mini, mini_bw: nail_image_10_mini_bw },
+    { full: nail_image_11, big: nail_image_11_big, medium: nail_image_11_medium, small: nail_image_11_small, mini: nail_image_11_mini, mini_bw: nail_image_11_mini_bw },
+    { full: nail_image_12, big: nail_image_12_big, medium: nail_image_12_medium, small: nail_image_12_small, mini: nail_image_12_mini, mini_bw: nail_image_12_mini_bw },
+    { full: nail_image_13, big: nail_image_13_big, medium: nail_image_13_medium, small: nail_image_13_small, mini: nail_image_13_mini, mini_bw: nail_image_13_mini_bw },
+    { full: nail_image_14, big: nail_image_14_big, medium: nail_image_14_medium, small: nail_image_14_small, mini: nail_image_14_mini, mini_bw: nail_image_14_mini_bw },
+    { full: nail_image_15, big: nail_image_15_big, medium: nail_image_15_medium, small: nail_image_15_small, mini: nail_image_15_mini, mini_bw: nail_image_15_mini_bw },
     ];
+
     const topImages = [11, 12];
     const bottomImages = [0, 3, 4];
     const images = imageNames.map((element, idx) => <GalleryImage key={element + idx} image={element} index={idx} enlarge={false} active={currentlyEnlarged} changeActive={imageSwitcher} top={topImages.includes(idx)} bottom={bottomImages.includes(idx)} />)
---
+++ d-nails/src/media/nail_photos/index.jsx
@@ -0,0 +1,108 @@
+import nail_image_01 from './nails_01.webp';
+import nail_image_01_big from './nails_01_big.webp';
+import nail_image_01_medium from './nails_01_medium.webp';
+import nail_image_01_small from './nails_01_small.webp';
+import nail_image_01_mini from './nails_01_mini.webp';
+import nail_image_01_mini_bw from './nails_01_mini_bw.webp';
+import nail_image_02 from './nails_02.webp';
+import nail_image_02_big from './nails_02_big.webp';
+import nail_image_02_medium from './nails_02_medium.webp';
+import nail_image_02_small from './nails_02_small.webp';
+import nail_image_02_mini from './nails_02_mini.webp';
+import nail_image_02_mini_bw from './nails_02_mini_bw.webp';
+import nail_image_03 from './nails_03.webp';
+import nail_image_03_big from './nails_03_big.webp';
+import nail_image_03_medium from './nails_03_medium.webp';
+import nail_image_03_small from './nails_03_small.webp';
+import nail_image_03_mini from './nails_03_mini.webp';
+import nail_image_03_mini_bw from './nails_03_mini_bw.webp';
+import nail_image_04 from './nails_04.webp';
+import nail_image_04_big from './nails_04_big.webp';
+import nail_image_04_medium from './nails_04_medium.webp';
+import nail_image_04_small from './nails_04_small.webp';
+import nail_image_04_mini from './nails_04_mini.webp';
+import nail_image_04_mini_bw from './nails_04_mini_bw.webp';
+import nail_image_05 from './nails_05.webp';
+import nail_image_05_big from './nails_05_big.webp';
+import nail_image_05_medium from './nails_05_medium.webp';
+import nail_image_05_small from './nails_05_small.webp';
+import nail_image_05_mini from './nails_05_mini.webp';
+import nail_image_05_mini_bw from './nails_05_mini_bw.webp';
+import nail_image_06 from './nails_06.webp';
+import nail_image_06_big from './nails_06_big.webp';
+import nail_image_06_medium from './nails_06_medium.webp';
+import nail_image_06_small from './nails_06_small.webp';
+import nail_image_06_mini from './nails_06_mini.webp';
+import nail_image_06_mini_bw from './nails_06_mini_bw.webp';
+import nail_image_07 from './nails_07.webp';
+import nail_image_07_big from './nails_07_big.webp';
+import nail_image_07_medium from './nails_07_medium.webp';
+import nail_image_07_small from './nails_07_small.webp';
+import nail_image_07_mini from './nails_07_mini.webp';
+import nail_image_07_mini_bw from './nails_07_mini_bw.webp';
+import nail_image_08 from './nails_08.webp';
+import nail_image_08_big from './nails_08_big.webp';
+import nail_image_08_medium from './nails_08_medium.webp';
+import nail_image_08_small from './nails_08_small.webp';
+import nail_image_08_mini from './nails_08_mini.webp';
+import nail_image_08_mini_bw from './nails_08_mini_bw.webp';
+import nail_image_09 from './nails_09.webp';
+import nail_image_09_big from './nails_09_big.webp';
+import nail_image_09_medium from './nails_09_medium.webp';
+import nail_image_09_small from './nails_09_small.webp';
+import nail_image_09_mini from './nails_09_mini.webp';
+import nail_image_09_mini_bw from './nails_09_mini_bw.webp';
+import nail_image_10 from './nails_10.webp';
+import nail_image_10_big from './nails_10_big.webp';
+import nail_image_10_medium from './nails_10_medium.webp';
+import nail_image_10_small from './nails_10_small.webp';
+import nail_image_10_mini from './nails_10_mini.webp';
+import nail_image_10_mini_bw from './nails_10_mini_bw.webp';
+import nail_image_11 from './nails_11.webp';
+import nail_image_11_big from './nails_11_big.webp';
+import nail_image_11_medium from './nails_11_medium.webp';
+import nail_image_11_small from './nails_11_small.webp';
+import nail_image_11_mini from './nails_11_mini.webp';
+import nail_image_11_mini_bw from './nails_11_mini_bw.webp';
+import nail_image_12 from './nails_12.webp';
+import nail_image_12_big from './nails_12_big.webp';
+import nail_image_12_medium from './nails_12_medium.webp';
+import nail_image_12_small from './nails_12_small.webp';
+import nail_image_12_mini from './nails_12_mini.webp';
+import nail_image_12_mini_bw from './nails_12_mini_bw.webp';
+import nail_image_13 from './nails_13.webp';
+import nail_image_13_big from './nails_13_big.webp';
+import nail_image_13_medium from './nails_13_medium.webp';
+import nail_image_13_small from './nails_13_small.webp';
+import nail_image_13_mini from './nails_13_mini.webp';
+import nail_image_13_mini_bw from './nails_13_mini_bw.webp';
+import nail_image_14 from './nails_14.webp';
+import nail_image_14_big from './nails_14_big.webp';
+import nail_image_14_medium from './nails_14_medium.webp';
+import nail_image_14_small from './nails_14_small.webp';
+import nail_image_14_mini from './nails_14_mini.webp';
+import nail_image_14_mini_bw from './nails_14_mini_bw.webp';
+import nail_image_15 from './nails_15.webp';
+import nail_image_15_big from './nails_15_big.webp';
+import nail_image_15_medium from './nails_15_medium.webp';
+import nail_image_15_small from './nails_15_small.webp';
+import nail_image_15_mini from './nails_15_mini.webp';
+import nail_image_15_mini_bw from './nails_15_mini_bw.webp';
+
+export {
+    nail_image_01, nail_image_01_big, nail_image_01_medium, nail_image_01_small, nail_image_01_mini, nail_image_01_mini_bw,
+    nail_image_02, nail_image_02_big, nail_image_02_medium, nail_image_02_small, nail_image_02_mini, nail_image_02_mini_bw,
+    nail_image_03, nail_image_03_big, nail_image_03_medium, nail_image_03_small, nail_image_03_mini, nail_image_03_mini_bw,
+    nail_image_04, nail_image_04_big, nail_image_04_medium, nail_image_04_small, nail_image_04_mini, nail_image_04_mini_bw,
+    nail_image_05, nail_image_05_big, nail_image_05_medium, nail_image_05_small, nail_image_05_mini, nail_image_05_mini_bw,
+    nail_image_06, nail_image_06_big, nail_image_06_medium, nail_image_06_small, nail_image_06_mini, nail_image_06_mini_bw,
+    nail_image_07, nail_image_07_big, nail_image_07_medium, nail_image_07_small, nail_image_07_mini, nail_image_07_mini_bw,
+    nail_image_08, nail_image_08_big, nail_image_08_medium, nail_image_08_small, nail_image_08_mini, nail_image_08_mini_bw,
+    nail_image_09, nail_image_09_big, nail_image_09_medium, nail_image_09_small, nail_image_09_mini, nail_image_09_mini_bw,
+    nail_image_10, nail_image_10_big, nail_image_10_medium, nail_image_10_small, nail_image_10_mini, nail_image_10_mini_bw,
+    nail_image_11, nail_image_11_big, nail_image_11_medium, nail_image_11_small, nail_image_11_mini, nail_image_11_mini_bw,
+    nail_image_12, nail_image_12_big, nail_image_12_medium, nail_image_12_small, nail_image_12_mini, nail_image_12_mini_bw,
+    nail_image_13, nail_image_13_big, nail_image_13_medium, nail_image_13_small, nail_image_13_mini, nail_image_13_mini_bw,
+    nail_image_14, nail_image_14_big, nail_image_14_medium, nail_image_14_small, nail_image_14_mini, nail_image_14_mini_bw,
+    nail_image_15, nail_image_15_big, nail_image_15_medium, nail_image_15_small, nail_image_15_mini, nail_image_15_mini_bw,
+};
